a798ba3a017e6516b49adddcadb53c1b494b0ba1
pipi <demo@demo.com>
2022-06-21 10:23:22 +0800

    typo

--- Readme.md
+++ Readme.md
@@ -1,4 +1,15 @@
+
+## compile && build docker image to docker daemon 
+`mvn clean compile`
+
+## deploy to k8s
 ```shell
-//compile && build docker image to docker daemon 
-mvn clean compile
+kubectl apply -f deploy/user.yaml
+kubectl apply -f deploy/dept.yaml
+```
+
+## testing
+```shell
+> configRefresh http://yourserver:30088/config
+> microservice http://yourserver:30088/userinfo
 ```
--- deptSer/src/main/java/com/example/MainController.java
+++ deptSer/src/main/java/com/example/MainController.java
@@ -1,8 +1,9 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.config.ConfigCloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,28 +13,26 @@ import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("/")
-public class IndexContro {
+public class MainController {
 
     @Resource
     private ConfigCloud config;
 
-    @Resource
-    private EmployeeClient employeeClient;
-
     @GetMapping("/config")
     public String load() {
-        return String.format(config.getFarewell() + " === " +  config.getGreeting(), "", "");
+        return String.format( "%s  === %s", config.getFarewell(), config.getGreeting());
     }
 
-    @GetMapping("userinfo")
-    public String usInfo(){
-        return employeeClient.findByDepartment("test").toString();
-    }
-
-    @RequestMapping()
+    @GetMapping("")
     String hello(@Value("${spring.application.name}") String name) throws UnknownHostException {
         return name + ": " + Inet4Address.getLocalHost().getHostAddress()
                 + "\n";
+    }
+
+    @GetMapping("/department/{departmentId}")
+    Employee get(@PathVariable("departmentId")String departmentId) throws UnknownHostException {
+        System.out.println("info: " + departmentId + "host: " + Inet4Address.getLocalHost().getHostAddress());
+        return new Employee(10L,departmentId,200,Inet4Address.getLocalHost().getHostAddress());
     }
 
 }
